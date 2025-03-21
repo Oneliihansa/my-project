d25a27c057bb91bc666cb1519db2de0821e08284
Oneli <oneli.ihansa@gmail.com>
2025-03-21 23:02:39 +0530

    square numbers added

--- program.py
+++ program.py
@@ -1,3 +1,8 @@
+def square_numbers(n):
+    for i in range(1, n + 1):
+        print(f"Square number {i}: {i ** 2}")
+
 def triangular_numbers(n):
     for i in range(1, n + 1):
         print(f"Triangular number {i}: {i * (i + 1) // 2}")
+
